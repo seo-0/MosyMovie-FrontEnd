6c5d83ae6fb50b87f7fe2e295515d49656830063
박서영(Tessa) <lilianapsy@naver.com>
2023-09-23 16:39:25 +0900

    Update: 회원가입 스타일링 수정

--- screens/LoginPage/RegisterScreen.js
+++ screens/LoginPage/RegisterScreen.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
-import { View, TextInput, Button, StyleSheet, Alert } from 'react-native';
+import { View, TextInput, StyleSheet, Switch, Text, Image, SafeAreaView, Modal, Alert, TouchableOpacity } from 'react-native';
 import axios from 'axios';
+import MosyMovie from '../images/MosyMovie.jpg';
 
 const RegisterScreen = ({ navigation }) => {
   const [emailId, setEmailId] = useState('');
@@ -8,6 +9,7 @@ const RegisterScreen = ({ navigation }) => {
   const [password, setPassword] = useState('');
   const [verificationCode, setVerificationCode] = useState('');
   const [isEmailVerified, setEmailVerified] = useState(false); // 이메일 인증 확인
+  const [registerModal, setRegisterModal] = useState(false);
 
   const handleRegister = async () => {
     try {
@@ -15,7 +17,6 @@ const RegisterScreen = ({ navigation }) => {
         emailId,
         name,
         password,
-        alarmReceive,
       });
       const data = response.data;
       
@@ -41,7 +42,6 @@ const RegisterScreen = ({ navigation }) => {
       console.log("이메일 인증 에러: ", error);
       Alert.alert("이메일 인증 에러", 'fail to send email verification');
     }
-   
   };
 
   const handleVerifyCode = async () => {
@@ -62,61 +62,182 @@ const RegisterScreen = ({ navigation }) => {
     }
   };
 
+  const handleLogin = async () => {
+    navigation.navigate('LoginScreen'); // 로그인 페이지로 이동
+  };
 
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.Text}>
+        <Text style={styles.TextStyle}>MosyMovie</Text>
+      </View>
       <TextInput
-        style={styles.input}
+        style={styles.textinput}
         placeholder="이메일"
         onChangeText={text => setEmailId(text)}
         value={emailId}
       />
       {!isEmailVerified && (
         <View>
-          <Button title="이메일 인증하기" onPress={handleEmailVerification} />
+          <TouchableOpacity
+            title="이메일 인증하기"
+            onPress={handleEmailVerification}
+            style={styles.emailVerificationButton} // 새로운 스타일 속성 추가
+          >
+            <Text style={styles.buttonText}>이메일 인증하기</Text>
+          </TouchableOpacity>
           <TextInput
-            style={styles.input}
+            style={styles.textinput}
             placeholder="인증 코드 입력"
             onChangeText={text => setVerificationCode(text)}
             value={verificationCode}
           />
-          <Button title="코드 인증" onPress={handleVerifyCode} />
-        </View>)}
+          <TouchableOpacity
+            title="코드 인증"
+            onPress={handleVerifyCode}
+            style={styles.codeVerificationButton} // 새로운 스타일 속성 추가
+          >
+            <Text style={styles.buttonText}>코드 인증</Text>
+          </TouchableOpacity>
+        </View>
+      )}
       <TextInput
-        style={styles.input}
+        style={styles.textinput}
         placeholder="사용자이름"
         onChangeText={text => setName(text)}
         value={name}
       />
       <TextInput
-        style={styles.input}
+        style={styles.textinput}
         placeholder="비밀번호"
         secureTextEntry
         onChangeText={text => setPassword(text)}
         value={password}
       />
-      <Button title="회원가입" onPress={handleRegister} />
-      <View style={{marginTop: 20}}>
-        <Text>아직 회원이 아니라면?</Text>
-        <Text 
-          style={{color: 'blue', textDecorationLine: 'underline'}}
-          onPress={() => navigation.navigate('LoginScreen')}>로그인 화면으로 이동</Text>
-      </View>
-    </View>
+      <TouchableOpacity
+        title="회원가입"
+        onPress={handleRegister}
+        style={styles.registerButton} // 새로운 스타일 속성 추가
+      >
+        <Text style={styles.buttonText}>회원가입</Text>
+      </TouchableOpacity>
+      <Modal
+        animationType='slide'
+        transparent={true}
+        visible={registerModal}
+      >
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <Text style={styles.modalText}>준비중 입니다.</Text>
+            <TouchableOpacity
+              style={styles.openButton}
+              onPress={() => {
+                setRegisterModal(!registerModal);
+              }}
+            >
+              <Text style={styles.textStyle}>Close</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </Modal>
+    </SafeAreaView>
   );
 };
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    alignItems: 'center',
     justifyContent: 'center',
-    padding: 16,
   },
-  input: {
+  Text: {
+    margin: 30,
+    borderRadius: 8,
+    backgroundColor: 'olive',
+    padding: 20,
+  },
+  TextStyle: {
+    textAlign: 'center',
+    color: 'navy',
+    fontSize: 60,
+    fontWeight: 'bold',
+  },
+  textinput: {
+    marginTop: 20,
     height: 40,
-    borderColor: 'gray',
+    width: 300,
+    borderColor: 'blue',
+    borderBottomWidth: 1,
+    marginBottom: 10,
+  },
+  emailVerificationButton: {
+    marginTop: 10,
+    backgroundColor: 'green',
+    borderRadius: 5,
+    paddingVertical: 12,
+    paddingHorizontal: 20,
     borderWidth: 1,
-    marginBottom: 16,
-    paddingLeft: 8,
+    borderColor: 'darkgreen',
+  },
+  codeVerificationButton: {
+    marginTop: 10,
+    backgroundColor: 'navy',
+    borderRadius: 5,
+    paddingVertical: 12,
+    paddingHorizontal: 20,
+    borderWidth: 1,
+    borderColor: 'darkblue',
+  },
+  registerButton: {
+    marginTop: 20,
+    backgroundColor: 'orangered',
+    borderRadius: 5,
+    paddingVertical: 12,
+    paddingHorizontal: 20,
+    borderWidth: 1,
+    borderColor: 'red',
+  },
+  buttonText: {
+    fontSize: 18,
+    color: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 22,
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: 'white',
+    borderRadius: 20,
+    padding: 35,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
+  openButton: {
+    backgroundColor: '#F194FF',
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+  },
+  textStyle: {
+    color: 'black',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: 'center',
   },
 });
 
